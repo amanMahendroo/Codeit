b15ae29767d1abb3e12dd7b9b24f1cb40ece8cd7
Aman Mahendroo <30946991+amanMahendroo@users.noreply.github.com>
2020-05-15 10:21:25 +0530

    Integration with Angular
    User and Post classes

---
+++ post.js
@@ -0,0 +1,8 @@
+class Post {
+	constructor(u, th, t) {
+		this.user = u
+		this.thumb = th
+		this.tags = t
+		this.vote = 0
+	}
+}
---
+++ user.js
@@ -0,0 +1,6 @@
+class User {
+	constructor(p, n) {
+		this.pfp = p
+		this.name = n
+	}
+}
