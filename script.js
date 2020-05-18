6cb0bf3743fad94820fa13b696b3bed32b35c4be
Aman Mahendroo <30946991+amanMahendroo@users.noreply.github.com>
2020-05-18 17:36:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,6 +16,24 @@ $(document).ready(function () {
 	$('.feed .new .code').click(function () {
 		window.location.href = "webeditor.html"
 	})
+
+	$('.ac').click(function () {
+		$('.account').css('z-index', 80)
+		$('.feed').css('z-index', 0)
+		$('.extra').css('z-index', 0)
+	})
+
+	$('.fd').click(function () {
+		$('.account').css('z-index', 0)
+		$('.feed').css('z-index', 80)
+		$('.extra').css('z-index', 0)
+	})
+
+	$('.xt').click(function () {
+		$('.account').css('z-index', 0)
+		$('.feed').css('z-index', 0)
+		$('.extra').css('z-index', 80)
+	})
 })
 
 function colors() {
@@ -23,12 +41,14 @@ function colors() {
 	let t = new Date().getHours()
 	if (t > 19 || t < 6) {
 		root.style.setProperty('--back', '#000')
+		root.style.setProperty('--shadow', '#fff3')
 		root.style.setProperty('--shade', '#111')
 		root.style.setProperty('--hover', '#222')
 		root.style.setProperty('--theme', '#a00')
 		root.style.setProperty('--text', '#fff')
 	} else {
 		root.style.setProperty('--back', '#fff')
+		root.style.setProperty('--shadow', '#0003')
 		root.style.setProperty('--shade', '#eee')
 		root.style.setProperty('--hover', '#ddd')
 		root.style.setProperty('--theme', '#a00')
@@ -38,5 +58,5 @@ function colors() {
 
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-    $scope.posts = []
+	$scope.posts = [1, 2, 3, 4, 5]
 });
