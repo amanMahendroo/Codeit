03b61e0e37e9de8d5bad430fe06874b3d291680f
Aman Mahendroo <30946991+amanMahendroo@users.noreply.github.com>
2020-05-26 13:42:03 +0530

    Login Page
    Finished login page UI

--- login.js
+++ login.js
@@ -5,6 +5,11 @@ $(document).ready(function () {
 		$('.second').css('display', 'block')
 	})
 
+	$('.back').click(function () {
+		$('.first').css('display', 'block')
+		$('.second').css('display', 'none')
+	})
+
 	$('.facebook, .google, .submit').click(function () {
 		window.location.href = "index.html"
 	})
@@ -13,15 +18,15 @@ $(document).ready(function () {
 function colors() {
 	let root = document.documentElement
 	let t = new Date().getHours()
-	if (t > 22 || t < 6) {
+	if (t > 19 || t < 6) {
 		root.style.setProperty('--back', '#000')
 		root.style.setProperty('--shade', '#fff1')
 		root.style.setProperty('--text', '#fff')
-		$('#logo').attr('src', 'dark.png')
+		$('#logo img').attr('src', 'dark.png')
 	} else {
 		root.style.setProperty('--back', '#fff')
 		root.style.setProperty('--shade', '#1111')
 		root.style.setProperty('--text', '#000')
-		$('#logo').attr('src', 'light.png')
+		$('#logo img').attr('src', 'light.png')
 	}
 }
